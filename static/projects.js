8eb15ad3dd6c83c3097a8df104a23f42a264e4d0
y77cao <y77cao@uwaterloo.ca>
2018-05-15 17:11:20 -0400

    Done

---
+++ static/projects.js
@@ -0,0 +1,48 @@
+export var Projects = [
+    {
+      "name": "giveMe\nSomething",
+      "description": "A movie suggestion web app.",
+      "download-link": "",
+      "gitHub-link": "",
+      "image": "givemesomething.png",
+      "hover": false
+    },
+    {
+      "name": "UWLife",
+      "description": "An accommodation platform for students. (coming soon stay tuned:P)",
+      "gitHub-link": "",
+      "image": "uwlife.png",
+      "hover": false
+    },
+    {
+      "name": "myStat",
+      "description": "An browser extension for analytics.",
+      "gitHub-link": "",
+      "image": "mystat.png",
+      "hover": false
+    },
+    {
+      "name": "colordoscope",
+      "description": "An intelligent image search engine.",
+      "download-link": "http://colordoscope.herokuapp.com",
+      "gitHub-link": "https://github.com/y77cao/Colordoscope",
+      "image": "colordoscope.png",
+      "hover": false
+    },
+    {
+      "name": "fTodo",
+      "description": "A very cool minimalism design iOS todo app.",
+      "download-link": "",
+      "gitHub-link": "",
+      "image": "ftodo.png",
+      "hover": false
+    },
+    {
+      "name": "personal site",
+      "description": "The thing you are looking at right now.",
+      "download-link": "",
+      "gitHub-link": "",
+      "image": "personalsite.png",
+      "hover": false
+    }
+];
